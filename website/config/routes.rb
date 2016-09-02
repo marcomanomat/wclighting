fbfa1017452a7cccb807210ed821b396eef68057
marcomanomat <marco.manomat@gmail.com>
2016-09-02 11:27:12 -0400

    edit manufacturer file names

--- website/config/routes.rb
+++ website/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-root 'construction#index'
+	root 'construction#index'
+  
   devise_for :admins
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/line-cards' => 'manufacturers#index'
+
+
+
+
 end
