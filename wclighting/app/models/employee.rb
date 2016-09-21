53fff9618ecf9e9ee73b38b95315a4c2e462f3ae
Kenny Phu <kphu@outlook.com>
2016-09-20 21:32:11 -0400

    Fix length validation, fix typo in manufacturer controller

--- wclighting/app/models/employee.rb
+++ wclighting/app/models/employee.rb
@@ -2,6 +2,6 @@ class Employee < ApplicationRecord
 	validates :first_name, :last_name, :title, :email, :phone, presence: true
 	validates :email, uniqueness: true
 	validates :first_name, :last_name, length: { maximum: 50 }
-	validates :phone, length: 10
-	
+	validates :phone, length: { is: 10 }
+
 end
