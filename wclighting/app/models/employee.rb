b40e52dc935ef4a3994efe98d64654d753c85cc0
marcomanomat <marco.manomat@gmail.com>
2016-09-21 14:35:03 -0400

    add :img table to Employee model

--- wclighting/app/models/employee.rb
+++ wclighting/app/models/employee.rb
@@ -4,4 +4,9 @@ class Employee < ApplicationRecord
 	validates :first_name, :last_name, length: { maximum: 50 }
 	validates :phone, length: { is: 10 }
 
+  validates_attachment_content_type :img,
+                                    :content_type => /^image\/(png|gif|jpeg)/,
+                                    :message => 'only (png/gif/jpeg) images',
+                                    :size => { in: 0..2.megabytes }		
+
 end
