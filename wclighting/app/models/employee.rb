cce539256b862eff59ab2bb5420ff497e7752975
marcomanomat <marco.manomat@gmail.com>
2016-10-25 19:29:27 -0400

    format padding and add s3 access to other models

--- wclighting/app/models/employee.rb
+++ wclighting/app/models/employee.rb
@@ -4,7 +4,16 @@ class Employee < ApplicationRecord
 	validates :first_name, :last_name, length: { maximum: 50 }
 	validates :phone, length: { is: 10 }
 
-	has_attached_file :img, required: false
+	has_attached_file :img, required: false,
+	              :style => { :medium => "300x300>", :thumb => "100x100>" },
+            :storage => :s3,
+            :bucket  => 'wclwebsite',
+            :s3_credentials => {
+		            :access_key_id => 'AKIAIWYJ72W3GHDHIMPQ',
+		            :secret_access_key => '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'
+                },
+            :s3_permissions => 'private',
+            :url => ":s3_domain_url"
 
   validates_attachment_content_type :img,
                                     :content_type => /^image\/(png|gif|jpeg)/,
