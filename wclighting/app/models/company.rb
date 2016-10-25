58c454ae01227561380919438e266a3adaf5ce1b
marcomanomat <marco.manomat@gmail.com>
2016-10-25 16:11:04 -0400

    fix company model for s3 uploads

--- wclighting/app/models/company.rb
+++ wclighting/app/models/company.rb
@@ -20,21 +20,22 @@ class Company < ApplicationRecord
 
 	validates :name, :phone, :address, :email, presence: true
 
-	has_attached_file :img, required: false
+	has_attached_file :img, required: false,
+              :style => { :medium => "300x300>", :thumb => "100x100>" },
+            :storage => :s3,
+            :bucket  => ENV['MY_BUCKET_NAME'],
+            :s3_credentials => {
+                    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+                    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+                },
+                :s3_permissions => 'private',
+                :url => ":s3_domain_url"
 
 
   validates_attachment_content_type :img,
                                     :content_type => /^image\/(png|gif|jpeg)/,
                                     :message => 'only (png/gif/jpeg) images',
                                     :size => { in: 0..2.megabytes }
-                                    :storage => :s3,
-                                    :bucket  => ENV['MY_BUCKET_NAME'],
-                                    :s3_credentials => {
-                                            :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-                                            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-                                        },
-                                        :s3_permissions => 'private',
-                                        :url => ":s3_domain_url"                                
 
 	accepts_nested_attributes_for :images, :allow_destroy => true                                   
 end
