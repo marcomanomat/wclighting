79e46037b262d6038c13552af9ceb712a80bcea5
marcomanomat <marco.manomat@gmail.com>
2016-10-25 21:57:57 -0400

    reconfigure manu

--- wclighting/app/models/manufacturer.rb
+++ wclighting/app/models/manufacturer.rb
@@ -23,27 +23,17 @@ class Manufacturer < ApplicationRecord
 	validates :name, :profile, :website, presence: true
 	validates :name, :website, uniqueness: true
 
-  has_attached_file :img, required: false,
-            :style => { :medium => "300x300>", :thumb => "100x100>" },
-            :storage => :s3,
-            :bucket  => 'wclwebsite',
-            :s3_credentials => {
-                    :access_key_id => 'AKIAIWYJ72W3GHDHIMPQ',
-                    :secret_access_key => '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'
-                },
-            :s3_permissions => 'private',
-            :url => ":s3_domain_url"
 
 	has_attached_file :logo, required: false,
-            :style => { :medium => "300x300>", :thumb => "100x100>" },
+                :style => { :medium => "300x300>", :thumb => "100x100>" },
             :storage => :s3,
             :bucket  => 'wclwebsite',
             :s3_credentials => {
                     :access_key_id => 'AKIAIWYJ72W3GHDHIMPQ',
                     :secret_access_key => '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'
                 },
-            :s3_permissions => 'private',
-            :url => ":s3_domain_url"
+                :s3_permissions => 'private',
+                :url => ":s3_domain_url"
 
   validates_attachment_content_type :logo,
                                     :content_type => /^image\/(png|gif|jpeg)/,
