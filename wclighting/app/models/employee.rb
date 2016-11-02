ad82fd0c10013f92e236d226dca4a75969b289cc
marcomanomat <marco.manomat@gmail.com>
2016-11-02 16:05:54 -0400

    format TEAM

--- wclighting/app/models/employee.rb
+++ wclighting/app/models/employee.rb
@@ -7,13 +7,13 @@ class Employee < ApplicationRecord
 	has_attached_file :img, required: false,
 	              :style => { :medium => "300x300>", :thumb => "100x100>" },
             :storage => :s3,
+            s3_region: 'us-east-1',
             :bucket  => 'wclwebsite',
             :s3_credentials => {
 		            :access_key_id => 'AKIAIWYJ72W3GHDHIMPQ',
 		            :secret_access_key => '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'
-                },
-            :s3_permissions => 'private',
-            :url => ":s3_domain_url"
+                }
+
 
   validates_attachment_content_type :img,
                                     :content_type => /^image\/(png|gif|jpeg)/,
